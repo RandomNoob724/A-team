ab3449af5fe6a2692d9ab4703614d3e27f17d5ca
Emil Persson <emilper1999@gmail.com>
2019-11-15 17:51:45 +0100

    Added privacy check if the user wan't to show current location or not

--- Mappy/Mappy/MapViewController.swift
+++ Mappy/Mappy/MapViewController.swift
@@ -9,16 +9,47 @@
 import UIKit
 import MapKit
 
+var allEvents: [Event] = []
+
 class MapViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        checkLocationServices()
         mapView.delegate = self
         let initialLocation = CLLocation(latitude: 57.7826, longitude: 14.1618)
         let mapEvent = Event(title: "This is an event", location: "Jönköping", description: "Local event", coordinates: CLLocationCoordinate2D(latitude: 57.7826, longitude: 14.1618))
         mapView.addAnnotation(mapEvent)
         centerMapOnLocation(location: initialLocation)
+    }
+    
+    func checkLocationServices(){
+        if CLLocationManager.locationServicesEnabled(){
+            checkLocationAuthorization()
+        } else {
+            print("User did not want to show location")
+        }
+    }
+    
+    func checkLocationAuthorization(){
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+        case .denied:
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            mapView.showsUserLocation = true
+        case .restricted:
+            break
+        case .authorizedAlways:
+            break
+        default:
+            mapView.showsUserLocation = false
+        }
     }
     
     func centerMapOnLocation(location: CLLocation){
